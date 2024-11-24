a40498c55c506b7bc4d56567e899db766271da34
LenuARG79 <lenuarg79@gmail.com>
2024-11-24 20:41:51 -0300

    V1.2

--- js/main.js
+++ js/main.js
@@ -18,3 +18,15 @@ function closeMenu() {
     menu.style.left = '-100%';
     hamburger.classList.remove('active');
 }
+
+// Detectar el enlace activo
+document.addEventListener("DOMContentLoaded", () => {
+    const currentPath = window.location.pathname;
+    const menuLinks = document.querySelectorAll("a.menu-link");
+
+    menuLinks.forEach(link => {
+        if (link.getAttribute("href") === currentPath) {
+            link.classList.add("active");
+        }
+    });
+});
